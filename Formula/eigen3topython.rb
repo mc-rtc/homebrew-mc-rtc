1ed79f2ba514c56f3f721285cfbade89d608e42e
Pierre Gergondet <pierre.gergondet@gmail.com>
2021-05-07 03:38:51 +0000

    eigen3topython 1.0.2 (new formula)
    Closes #1.
    
    Signed-off-by: Pierre Gergondet <473543+gergondet@users.noreply.github.com>

---
+++ Formula/eigen3topython.rb
@@ -0,0 +1,48 @@
+class Eigen3topython < Formula
+  desc "Provide Eigen3 to numpy conversion"
+  homepage "https://github.com/jrl-umi3218/Eigen3ToPython"
+  url "https://github.com/jrl-umi3218/Eigen3ToPython/releases/download/v1.0.2/Eigen3ToPython-v1.0.2.tar.gz"
+  sha256 "36b4462e7a924eee0dc8462ce56e5cc58e7196d9b6b4732750462fb507934de0"
+  license "BSD-2-Clause"
+
+  depends_on "cmake" => :build
+  depends_on "cython" => :build
+  depends_on "eigen" => :build
+  depends_on "numpy"
+  depends_on "python"
+
+  # The patch removes the requirement on coverage and nose which are not needed here
+  patch :DATA
+
+  def install
+    xy = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
+    ENV.prepend_create_path "PYTHONPATH", Formula["cython"].opt_libexec/"lib/python#{xy}/site-packages"
+
+    args = std_cmake_args + %W[
+      -DPIP_INSTALL_PREFIX=#{prefix}
+      -DPYTHON_BINDING_FORCE_PYTHON3:BOOL=ON
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    system Formula["python"].opt_bin/"python3", "-c", <<~EOS
+      import eigen
+      print("Eigen version: {}".format(eigen.EigenVersion()))
+      print("Random Vector3d: {}".format(eigen.Vector3d.Random().transpose()))
+    EOS
+  end
+end
+__END__
+diff --git a/requirements.txt b/requirements.txt
+index 1815ab1..a4ce253 100644
+--- a/requirements.txt
++++ b/requirements.txt
+@@ -1,4 +1,2 @@
+ Cython>=0.2
+-coverage
+-nose
+ numpy>=1.8.2

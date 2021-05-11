d7f991604485332df36ff9ad67fc2064d913ba11
Pierre Gergondet <473543+gergondet@users.noreply.github.com>
2021-05-11 15:04:50 +0000

    mc_rtc_data: add 1.0.4 bottle.

--- Formula/mc_rtc_data.rb
+++ Formula/mc_rtc_data.rb
@@ -5,6 +5,12 @@ class McRtcData < Formula
   sha256 "682c0bff88970668567c9f06f5bfad57032b48ba488e66ae5466b31d49b77f3f"
   license "BSD-2-Clause"
 
+  bottle do
+    root_url "https://github.com/mc-rtc/homebrew-mc-rtc/releases/download/mc_rtc_data-1.0.4"
+    sha256 cellar: :any_skip_relocation, catalina:     "19776a56170605ee5e79365cd6d6220fe684d75ba7bf50dc0f32db3ae5ccb6b5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "f3ad769db551b0911df9e779daef4225e6da1d36661a7fa137cc0b179e35706a"
+  end
+
   depends_on "cmake" => [:build, :test]
 
   def install

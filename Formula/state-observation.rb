df26b72bbb3e99446b034777da2401fb29b99480
Pierre Gergondet <pierre.gergondet@gmail.com>
2021-05-11 23:01:46 +0800

    state-observation 1.4.1 (new formula)

---
+++ Formula/state-observation.rb
@@ -0,0 +1,46 @@
+class StateObservation < Formula
+  desc "Describes interfaces for state observers, and implements some observers"
+  homepage "https://github.com/jrl-umi3218/state-observation/"
+  url "https://github.com/jrl-umi3218/state-observation/releases/download/v1.4.1/state-observation-v1.4.1.tar.gz"
+  sha256 "12707b491a98be8cf9aad68469683610ed80691a119f3e4643a99db8e95c72be"
+  license "BSD-2-Clause"
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "boost"
+  depends_on "eigen"
+
+  resource "test.cpp" do
+    url "https://raw.githubusercontent.com/jrl-umi3218/state-observation/v1.4.1/unit-testings/test_acceleration_stabilization.cpp"
+    sha256 "f10d373523eed1ea3908ab36383286bbc1ec9d77e867173e4b38c0181b16701d"
+  end
+
+  def install
+    ENV["HOMEBREW_ARCHFLAGS"] = "-march=#{Hardware.oldest_cpu}" unless build.bottle?
+
+    args = std_cmake_args + %w[
+      -DINSTALL_DOCUMENTATION:BOOL=OFF
+      -DBUILD_STATE_OBSERVATION_TOOLS:BOOL=OFF
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    resource("test.cpp").stage testpath
+    (testpath/"CMakeLists.txt").write <<~EOS
+      cmake_minimum_required(VERSION 3.1)
+      set(CMAKE_CXX_STANDARD 11)
+      project(Brewstate-observation LANGUAGES CXX)
+      find_package(state-observation REQUIRED)
+      add_executable(main test_acceleration_stabilization.cpp)
+      target_link_libraries(main PUBLIC state-observation::state-observation)
+    EOS
+    # Avoid introducing march=native which will cause ABI breaks
+    ENV["CXXFLAGS"] = ""
+    system "cmake", ".", *std_cmake_args
+    system "cmake", "--build", "."
+    system "./main"
+  end
+end

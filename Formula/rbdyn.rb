61783af014e91df0a6741b034f96bacaeacf9210
Pierre Gergondet <pierre.gergondet@gmail.com>
2021-06-28 11:32:33 +0800

    rbdyn 1.5.1 (update formula)
    Closes #19.
    
    Signed-off-by: Pierre Gergondet <473543+gergondet@users.noreply.github.com>

--- Formula/rbdyn.rb
+++ Formula/rbdyn.rb
@@ -1,10 +1,9 @@
 class Rbdyn < Formula
   desc "Classes and functions to model the dynamics of rigid body systems"
   homepage "https://github.com/jrl-umi3218/RBDyn"
-  url "https://github.com/jrl-umi3218/RBDyn/releases/download/v1.5.0/RBDyn-v1.5.0.tar.gz"
-  sha256 "d018f57a354a4ea8e0cd50c2fdf018f9a510ff8cc34a12d12cff793120458e5b"
+  url "https://github.com/jrl-umi3218/RBDyn/releases/download/v1.5.1/RBDyn-v1.5.1.tar.gz"
+  sha256 "e4eac39f558b28dcaeb3f62e26e28e2888486b8b122bda6ffd6b966903dbf0d7"
   license "BSD-2-Clause"
-  revision 1
 
   bottle do
     root_url "https://github.com/mc-rtc/homebrew-mc-rtc/releases/download/rbdyn-1.5.0_1"

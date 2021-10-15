cb794b3a119bf7dcca5703cd9a49b0eff1ef0d72
Pierre Gergondet <473543+gergondet@users.noreply.github.com>
2021-10-15 13:25:27 +0000

    mc_rtc: update 1.8.2 bottle.

--- Formula/mc_rtc.rb
+++ Formula/mc_rtc.rb
@@ -6,9 +6,9 @@ class McRtc < Formula
   license "BSD-2-Clause"
 
   bottle do
-    root_url "https://github.com/mc-rtc/homebrew-mc-rtc/releases/download/mc_rtc-1.8.1"
-    sha256 catalina:     "5d6d122ec5e6c64be45c3ad521be53366eaf8cb78fbaffb6e4d27ae272748db5"
-    sha256 x86_64_linux: "a332bef277ef1d76906426eedd49ec3f21ce11dd20ca92f563e8272c3a306f38"
+    root_url "https://github.com/mc-rtc/homebrew-mc-rtc/releases/download/mc_rtc-1.8.2"
+    sha256 catalina:     "4d16d81b4afd3b54f1b5d451796cf888216f17a7e8f1adf99d2993e6c3da2d96"
+    sha256 x86_64_linux: "852868da270f6c9c99544a5ef8f9fd195c3eec230ff3c9fedb2232ed24719321"
   end
 
   depends_on "cmake" => [:build, :test]

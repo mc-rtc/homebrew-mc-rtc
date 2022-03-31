04e64ce2a0c400aac2bab1e335976776ba1552de
Pierre Gergondet <473543+gergondet@users.noreply.github.com>
2022-03-31 09:36:58 +0000

    spacevecalg: update 1.2.1 bottle.

--- Formula/spacevecalg.rb
+++ Formula/spacevecalg.rb
@@ -6,9 +6,8 @@ class Spacevecalg < Formula
   license "BSD-2-Clause"
 
   bottle do
-    root_url "https://github.com/mc-rtc/homebrew-mc-rtc/releases/download/spacevecalg-1.2.0"
-    sha256 cellar: :any_skip_relocation, catalina:     "66a06b1b7fce83095e15b3d547cfa6057f32f5df31b011467ea33e39b4c5b937"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "0efd3c834439f055bd406909a9b9be0cd45584b732168a7067e77c6bd1024726"
+    root_url "https://github.com/mc-rtc/homebrew-mc-rtc/releases/download/spacevecalg-1.2.1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "7f81a825c60fe81cc4e281f7e87fe27b80ced9b82487130a7fa9ead182967cef"
   end
 
   depends_on "cmake" => [:build, :test]

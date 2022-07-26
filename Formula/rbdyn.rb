fbe6b8a59062ca9b5789e0db7803e0c45e445da5
Pierre Gergondet <pierre.gergondet@gmail.com>
2022-07-26 11:05:51 +0200

    rbdyn 1.6.0
    * rbdyn 1.6.0
    * rbdyn: bump revision
      Update tarball sha256

--- Formula/rbdyn.rb
+++ Formula/rbdyn.rb
@@ -1,10 +1,10 @@
 class Rbdyn < Formula
   desc "Classes and functions to model the dynamics of rigid body systems"
   homepage "https://github.com/jrl-umi3218/RBDyn"
-  url "https://github.com/jrl-umi3218/RBDyn/releases/download/v1.5.2/RBDyn-v1.5.2.tar.gz"
-  sha256 "80718425f430aa215551fb74e6dd383a26f31ee6792ea587c3266fc81988b8bc"
+  url "https://github.com/jrl-umi3218/RBDyn/releases/download/v1.6.0/RBDyn-v1.6.0.tar.gz"
+  sha256 "2fc0ad358437350e0051fdca4fb67646ffbe8d49662266fc548cca26c9dc16c1"
   license "BSD-2-Clause"
-  revision 2
+  revision 1
 
   bottle do
     root_url "https://github.com/mc-rtc/homebrew-mc-rtc/releases/download/rbdyn-1.5.2_2"

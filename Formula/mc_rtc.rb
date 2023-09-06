63b8190786325d8823023b680c8b22a5a1985fa4
Pierre Gergondet <pierre.gergondet@gmail.com>
2023-09-06 12:30:03 +0900

    [mc_rtc] Update to 2.7.0

--- Formula/mc_rtc.rb
+++ Formula/mc_rtc.rb
@@ -1,8 +1,8 @@
 class McRtc < Formula
   desc "Interface for simulated and real robotic systems suitable for real-time control"
   homepage "https://jrl-umi3218.github.io/mc_rtc/"
-  url "https://github.com/jrl-umi3218/mc_rtc/releases/download/v2.6.0/mc_rtc-v2.6.0.tar.gz"
-  sha256 "85d03113821f8fe3241e7c2755345477b62242a362f9769f107483d74af9beb4"
+  url "https://github.com/jrl-umi3218/mc_rtc/releases/download/v2.7.0/mc_rtc-v2.7.0.tar.gz"
+  sha256 "cafb986c89243591d521ee12031db3aa692bb44ccf778ffa4a296c0f75eed2ac"
   license "BSD-2-Clause"
   revision 0
 
